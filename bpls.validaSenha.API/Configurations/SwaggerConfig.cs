f4f0c2dd896b319ef7dd17eed44eef9e3d4c083a
BrunoLSouza <bruno.pinheiro1990@gmail.com>
2021-11-18 21:12:22 -0300

    adicionado swagger

---
+++ bpls.validaSenha.API/Configurations/SwaggerConfig.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bpls.validaSenha.API.Configurations
+{
+    public static class SwaggerConfig
+    {
+        public static void AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo { 
+                    Version = "v1",
+                    Title = "API Valida Senha",
+                    Description = "API de Validação de Senha"
+                });
+            
+            });
+
+
+        }
+
+        public static void UseSwaggerSetup(this IApplicationBuilder app)
+        {
+            if (app == null) throw new ArgumentNullException(nameof(app));
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                string swaggerJsonBasePath = string.IsNullOrWhiteSpace(c.RoutePrefix) ? "." : "..";
+                c.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/v1/swagger.json","v1");
+            });
+
+        }
+
+
+    }
+}
--- bpls.validaSenha.API/Startup.cs
+++ bpls.validaSenha.API/Startup.cs
@@ -1,3 +1,4 @@
+using bpls.validaSenha.API.Configurations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,7 @@ namespace bpls.validaSenha.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerConfiguration();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +48,8 @@ namespace bpls.validaSenha.API
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwaggerSetup();
         }
     }
 }
